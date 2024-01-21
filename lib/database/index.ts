132c648cd94ef6127993ee88eaeae24a05bf6b06
Kailash Kumar <kailash@kailashs-MacBook-Air.local>
2024-01-21 15:33:40 +0530

    FileName

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -5,16 +5,19 @@ const MONGODB_URI = process.env.MONGODB_URI;
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-    if(cached.conn) return cached.connect;
+  if (cached.conn) return cached.conn;
 
-    if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  console.log(MONGODB_URI);
+  
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName: 'evently',
-        bufferCommands: false,
-    })
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-    cached.conn = await cached.promise;
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'evently',
+    bufferCommands: false,
+  })
 
-    return cached.conn;
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
